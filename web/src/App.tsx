b02dad31aabb2e7bed0f87f66e491a142ad85768
Nicola Miotto <miotto@posteo.de>
2024-08-19 16:30:00 +0200

    add form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,42 @@
 import {
-  Box,
   Button,
   Flex,
   Heading,
   HStack,
   Spacer,
+  Text,
   VStack,
 } from "@chakra-ui/react";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
 import ConnectButton from "./ConnectButton";
+import Example from "./Example";
+import Create from "./Create";
 
 function App() {
   return (
-    <VStack>
+    <VStack height="100vh">
       <Flex flexDir="row" width="100%" padding="10px">
         <Button variant="primary">ABOUT</Button>
         <Spacer />
         <ConnectButton />
       </Flex>
-      <Heading as="h1">Propcorn</Heading>
+      <Heading as="h1" size="4xl">
+        Propcorn
+      </Heading>
+      <Spacer />
+      <Heading as="h2" size="xl">
+        Get funds for your opensource work.
+      </Heading>
+      <Spacer />
+      <VStack>
+        <Heading as="h2">Examples</Heading>
+      </VStack>
+      <HStack>
+        <Example />
+        <Example />
+        <Example />
+      </HStack>
+      <Spacer />
+      <Create />
     </VStack>
   );
 }
---
+++ web/src/Example.tsx
@@ -0,0 +1,17 @@
+import { Box, Text } from "@chakra-ui/react";
+
+function Example() {
+  return (
+    <Box padding="5px" borderRadius="5px" borderColor="white" borderWidth="1px">
+      <Text fontSize="15px" fontWeight="bold">
+        Proposal #1
+      </Text>
+      <Text fontSize="10px">Aknowledgement system....</Text>
+      <Text fontSize="10px" color="lightgrey">
+        vrde.eth
+      </Text>
+    </Box>
+  );
+}
+
+export default Example;
