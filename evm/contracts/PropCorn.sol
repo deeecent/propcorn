c2f843d351b59e4aeba152e4e22b3414f4bc4dcb
Alberto Granzotto <alberto@granzotto.net>
2024-08-19 16:22:50 +0200

    First simple interation on contract

--- evm/contracts/PropCorn.sol
+++ evm/contracts/PropCorn.sol
@@ -5,11 +5,34 @@ contract PropCorn {
     struct Proposal {
         string url;
         uint256 daysToUnlock;
-        uint256 amountRequested;
+        uint256 minAmountRequested;
         uint256 balance;
+        bool finished;
     }
+
+    mapping(address => Proposal[]) internal _proposals;
 
     constructor() {}
 
-    function createProposal(uint256 daysToUnlock) public {}
+    function createProposal(Proposal memory proposal) public {}
+
+    function fundProposal(address account, uint256 id) public payable {}
+
+    function withdrawFunds(address user) public {}
+
+    function getProposalByAccount(address account, uint256 index) public view returns (Proposal memory) {
+        return _proposals[account][index];
+    }
+
+    receive() external payable {}
+
+    /* 
+      - Write:
+        - create a proposal
+        - fund a proposal with eth
+        - redeem funds
+      - Read:
+        - get a proposal by address + proposal id
+    // function listProposalsByAccount(address account, uint256 page) public view returns (Proposal[50] memory) {}
+    */
 }
