f0752c41f8ab99062a42bebbefa873d8d761e7e8
joshschcom <joshuaschiemann@yahoo.de>
2024-08-20 17:53:45 +0200

    Added info on new task

--- evm/tasks/ccippropcorn.ts
+++ evm/tasks/ccippropcorn.ts
@@ -1,15 +1,14 @@
 import { task } from "hardhat/config";
 
-task("deploy:propcornccip", "Deploy Propcornccip Contract").setAction(async function (
-  _,
-  { ethers },
-) {
-  const [deployer] = await ethers.getSigners();
-  const ccipPropcornFactory = await ethers.getContractFactory("Propcornccip");
-  console.log("Deploying Propcorn");
-  const propcornccip = await ccipPropcornFactory
-    .connect(deployer)
-    .deploy("CHANGETHIS",deployer.address); //https://docs.chain.link/ccip/supported-networks
-  await propcornccip.waitForDeployment();
-  console.log("Propcornccip deployed to: ", await propcornccip.getAddress());
-});
+task("deploy:propcornccip", "Deploy Propcornccip Contract").setAction(
+  async function (_, { ethers }) {
+    const [deployer] = await ethers.getSigners();
+    const ccipPropcornFactory = await ethers.getContractFactory("Propcornccip");
+    console.log("Deploying Propcorn");
+    const propcornccip = await ccipPropcornFactory
+      .connect(deployer)
+      .deploy("CHANGETHIS", deployer.address); //fill in the Router address from https://docs.chain.link/ccip/supported-networks
+    await propcornccip.waitForDeployment();
+    console.log("Propcornccip deployed to: ", await propcornccip.getAddress());
+  },
+);
