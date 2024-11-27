4cef0db159a17e6709a490badefebc10b77b7f0c
Nicola Miotto <miotto@posteo.de>
2024-11-27 11:32:48 +0100

    simplify hardhat config + add upgrades support

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,55 +1,16 @@
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy";
 import type { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
-import type { NetworkUserConfig } from "hardhat/types";
 
 import "./tasks/accounts";
 import "./tasks/propcorn";
 
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
-
-const mnemonic: string = vars.get("MNEMONIC");
-const alchemyApiKey: string = vars.get("ALCHEMY_API_KEY");
-
-const chainIds = {
-  "arbitrum-mainnet": 42161,
-  avalanche: 43114,
-  bsc: 56,
-  ganache: 1337,
-  hardhat: 31337,
-  mainnet: 1,
-  "optimism-mainnet": 10,
-  "polygon-mainnet": 137,
-  "polygon-mumbai": 80001,
-  sepolia: 11155111,
-};
-
-function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
-  let jsonRpcUrl: string;
-  switch (chain) {
-    case "avalanche":
-      jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
-      break;
-    case "bsc":
-      jsonRpcUrl = "https://bsc-dataseed1.binance.org";
-      break;
-    case "optimism-mainnet":
-      jsonRpcUrl = "https://opt-mainnet.g.alchemy.com/v2/" + alchemyApiKey;
-      break;
-    default:
-      jsonRpcUrl = "https://eth-sepolia.g.alchemy.com/v2/" + alchemyApiKey;
-  }
-  return {
-    accounts: {
-      count: 10,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
-    chainId: chainIds[chain],
-    url: jsonRpcUrl,
-  };
-}
+const NETWORK: string = vars.get("NETWORK");
+const RPC_URL: string = vars.get("RPC_URL");
+const PRIVATE_KEY: string = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -58,13 +19,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      arbitrumOne: vars.get("ARBISCAN_API_KEY", ""),
-      avalanche: vars.get("SNOWTRACE_API_KEY", ""),
-      bsc: vars.get("BSCSCAN_API_KEY", ""),
-      mainnet: vars.get("ETHERSCAN_API_KEY", ""),
       optimisticEthereum: vars.get("OPTIMISM_API_KEY", ""),
-      polygon: vars.get("POLYGONSCAN_API_KEY", ""),
-      polygonMumbai: vars.get("POLYGONSCAN_API_KEY", ""),
       sepolia: vars.get("ETHERSCAN_API_KEY", ""),
     },
   },
@@ -75,27 +30,11 @@ const config: HardhatUserConfig = {
     src: "./contracts",
   },
   networks: {
-    hardhat: {
-      accounts: {
-        mnemonic,
-      },
-      chainId: chainIds.hardhat,
+    hardhat: {},
+    [NETWORK]: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
     },
-    ganache: {
-      accounts: {
-        mnemonic,
-      },
-      chainId: chainIds.ganache,
-      url: "http://localhost:8545",
-    },
-    arbitrum: getChainConfig("arbitrum-mainnet"),
-    avalanche: getChainConfig("avalanche"),
-    bsc: getChainConfig("bsc"),
-    mainnet: getChainConfig("mainnet"),
-    optimism: getChainConfig("optimism-mainnet"),
-    "polygon-mainnet": getChainConfig("polygon-mainnet"),
-    "polygon-mumbai": getChainConfig("polygon-mumbai"),
-    sepolia: getChainConfig("sepolia"),
   },
   paths: {
     artifacts: "./artifacts",
