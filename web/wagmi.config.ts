d5004cf61ebf371d34b731a92b441db57b0e0790
Nicola Miotto <miotto@posteo.de>
2024-12-11 10:51:52 +0100

    exlude testing contracts from wagmi generate

--- web/wagmi.config.ts
+++ web/wagmi.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "@wagmi/cli";
-import { HardhatConfig, hardhat } from "@wagmi/cli/plugins";
+import { hardhat } from "@wagmi/cli/plugins";
 import { react } from "@wagmi/cli/plugins";
 
 export default defineConfig({
   out: "src/generated.ts",
-  contracts: [],
   plugins: [
     hardhat({
       project: "../evm",
+      exclude: [
+        // the following patterns are excluded by default
+        "testing/**",
+      ],
       deployments: {
         Propcorn: {
           10: "0xa1430c9c87D3c534EEfC429F4F29577282b8dE0F",
