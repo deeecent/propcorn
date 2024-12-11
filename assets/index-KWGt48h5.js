a2cb4649aad24ef9d39fe2a09dc6e59dca274533
Nicola Miotto <miotto@posteo.de>
2024-12-11 17:32:48 +0100

    Updates

--- assets/index-KWGt48h5.js
+++ assets/index-KWGt48h5.js
@@ -1,4 +1,4 @@
-import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-BKRWbhh3.js";import{r as ho,f as mo}from"./index-1vWj7hTf.js";/**
+import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-CQR-rLOy.js";import{r as ho,f as mo}from"./index-DCuIl9Ie.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
