37569a6af2f0a5e38ee32b8000ae2cbfbfb896bd
Alberto Granzotto <alberto@granzotto.net>
2024-12-11 13:31:31 +0100

    Add ENS resolution

--- web/src/Proposal.tsx
+++ web/src/Proposal.tsx
@@ -24,27 +24,35 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import { useWaitForTransactionReceipt } from "wagmi";
-import { useWritePropcornFundProposal } from "./generated";
+import { useAccount, useWaitForTransactionReceipt } from "wagmi";
+import {
+  useReadPropcornFunderToProposalBalance,
+  useWritePropcornDefundProposal,
+  useWritePropcornFundProposal,
+  useWritePropcornWithdrawFunds,
+} from "./generated";
 import { useEffect, useState } from "react";
-import { formatEther, formatUnits, parseEther } from "viem";
+import { formatEther, formatUnits, Hex, parseEther } from "viem";
 import { convertSecondsToDaysAndHours } from "./utils";
 import { GitHubIssueData } from "./github";
 import Link from "./Link";
 import Markdown from "react-markdown";
+import AddressInput from "./AddressInput";
+
+type Proposal = {
+  index: bigint;
+  url: string;
+  secondsToUnlock: bigint;
+  fundingCompletedAt: bigint;
+  minAmountRequested: bigint;
+  balance: bigint;
+  feeBasisPoints: bigint;
+  author: `0x${string}`;
+  status: number;
+};
 
 type ProposalProps = {
-  proposal: {
-    index: bigint;
-    url: string;
-    secondsToUnlock: bigint;
-    fundingCompletedAt: bigint;
-    minAmountRequested: bigint;
-    balance: bigint;
-    feeBasisPoints: bigint;
-    author: `0x${string}`;
-    status: number;
-  };
+  proposal: Proposal;
   issue: GitHubIssueData;
 };
 
@@ -142,7 +150,177 @@ const FundProposal = ({ proposal, issue }: ProposalProps) => {
   );
 };
 
+type DefundProposalProps = {
+  index: bigint;
+  address: Hex;
+};
+
+const DefundProposal = ({ index, address }: DefundProposalProps) => {
+  const { data: funding } = useReadPropcornFunderToProposalBalance({
+    args: [address, index],
+  });
+  const { data: hash, writeContract } = useWritePropcornDefundProposal();
+  const { isLoading, isError, isSuccess } = useWaitForTransactionReceipt({
+    hash,
+  });
+  const toast = useToast();
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast({
+        title: "Transaction successful",
+        description:
+          "Your Ether has been successfully returned to your wallet 🫡",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      toast({
+        title: "Something went wrong",
+        description: "Please check your wallet and try again 🫣",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [isError]);
+
+  const onDefundProposal = () => {
+    toast({
+      title: "Check your wallet",
+      description: "Review the transaction details and confirm to proceed 🤓",
+      status: "info",
+      duration: 4000,
+      isClosable: true,
+    });
+    writeContract({ args: [index] });
+  };
+
+  if (!funding || funding === 0n) {
+    return null;
+  }
+
+  return (
+    <Box w="full">
+      <Text color="gray.500" fontSize="sm" mb={2}>
+        You have funded {formatEther(funding)} Ether
+      </Text>
+      <Button w="full" onClick={onDefundProposal} disabled={isLoading}>
+        Defund Proposal
+      </Button>
+    </Box>
+  );
+};
+
+const WithdrawFunds = ({
+  index,
+  issue,
+  defaultReceiver,
+}: {
+  index: bigint;
+  issue: GitHubIssueData;
+  defaultReceiver?: Hex | undefined;
+}) => {
+  const { data: hash, writeContract } = useWritePropcornWithdrawFunds();
+  const { isLoading, isError, isSuccess } = useWaitForTransactionReceipt({
+    hash,
+  });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+  const [address, setAddress] = useState(defaultReceiver);
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast({
+        title: "Transaction successful",
+        description: "Ether transferred successfully 🫡",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      toast({
+        title: "Something went wrong",
+        description: "Please check your wallet and try again 🫣",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [isError]);
+
+  const onWithdrawFunds = () => {
+    toast({
+      title: "Check your wallet",
+      description: "Review the transaction details and confirm to proceed 🤓",
+      status: "info",
+      duration: 4000,
+      isClosable: true,
+    });
+    writeContract({ args: [index, address as Hex] });
+  };
+
+  return (
+    <>
+      <Button onClick={onOpen} colorScheme="yellow">
+        Withdraw funds
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Heading as="h4" fontSize="large">
+              Withdraw funds for Proposal #{index.toString()}
+            </Heading>
+            <Text as="span" fontSize="md" mr={1} color="gray.500">
+              {issue.title}
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl mt={4}>
+              <FormLabel>Withdraw to</FormLabel>
+              <AddressInput
+                onAddressChange={setAddress}
+                value={defaultReceiver}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            {isSuccess ? (
+              <Button colorScheme="yellow" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            ) : (
+              <Button
+                colorScheme="green"
+                mr={3}
+                onClick={onWithdrawFunds}
+                disabled={isLoading}
+              >
+                Withdraw funds
+              </Button>
+            )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
 const Proposal = ({ proposal, issue }: ProposalProps) => {
+  const { address } = useAccount();
   const { days, hours } = convertSecondsToDaysAndHours(
     Number(proposal.secondsToUnlock),
   );
@@ -207,7 +385,17 @@ const Proposal = ({ proposal, issue }: ProposalProps) => {
 
         <Spacer />
 
-        <FundProposal proposal={proposal} issue={issue} />
+        <Stack gap={5}>
+          <FundProposal proposal={proposal} issue={issue} />
+          {address && (
+            <DefundProposal index={proposal.index} address={address} />
+          )}
+          <WithdrawFunds
+            index={proposal.index}
+            issue={issue}
+            defaultReceiver={address}
+          />
+        </Stack>
       </HStack>
 
       <Card>
