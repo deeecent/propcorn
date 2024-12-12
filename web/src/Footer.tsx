c22e4c0a407a0a5714832319c8cf717faab2e4db
Alberto Granzotto <alberto@granzotto.net>
2024-12-12 11:36:03 +0100

    Create Telegram group Fixes #12

--- web/src/Footer.tsx
+++ web/src/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "./Link";
 
 const Footer = () => {
   return (
-    <Box as="footer" py={4} px={8}>
+    <Box as="footer" py={4} px={8} fontSize="xs">
       <Stack
         direction={{ base: "column", sm: "row" }}
         spacing={4}
@@ -13,11 +13,14 @@ const Footer = () => {
         <Link to="/about">About</Link>
         <Link to="/privacy-policy">Privacy Policy</Link>
         <Link to="/terms-of-use">Terms of Use</Link>
+        <Link to="https://t.me/propcornprotocol" isExternal>
+          Telegram Group
+        </Link>
         <Link to="https://github.com/deeecent/propcorn" isExternal>
           Source Code
         </Link>
       </Stack>
-      <Text mt={4} textAlign="center" fontSize="sm">
+      <Text mt={4} textAlign="center">
         &copy; {new Date().getFullYear()} deeecent. All rights reserved.
       </Text>
     </Box>
