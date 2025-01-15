b9d49627cea3f26de5cc7bfff9aab64a3552b52b
Nicola Miotto <miotto@posteo.de>
2025-01-15 14:46:05 +0100

    add seeding task (for testnet)

--- evm/tasks/admin.ts
+++ evm/tasks/admin.ts
@@ -20,6 +20,33 @@ task("proposal:create", "Create a proposal", async (_, hre) => {
   await tx.wait(1);
 });
 
+task("proposal:seed", "Seed proposals")
+  .addPositionalParam("amount", "Number of proposals")
+  .setAction(async ({ amount }: { amount: number }, hre) => {
+    const { chainId } = await hre.ethers.provider.getNetwork();
+    if (chainId === 10n) {
+      console.error("Not on mainnet!");
+      return;
+    }
+
+    const propcorn = (await loadContract(
+      hre,
+      "Propcorn",
+      "PropcornModule#Proxy",
+    )) as unknown as Propcorn;
+
+    for (let i = 0; i < amount; i++) {
+      const tx = await propcorn.createProposal(
+        "https://github.com/deeecent/propcorn/issues/46",
+        60,
+        parseEther(`0.0${i}`),
+        100,
+      );
+
+      await tx.wait(1);
+    }
+  });
+
 task("proposal:get", "Get a proposal", async (_, hre) => {
   const propcorn = (await loadContract(
     hre,
