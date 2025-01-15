0c6139848483179e60f4a3fca515de6dc23848f1
Nicola Miotto <miotto@posteo.de>
2025-01-15 18:24:36 +0100

    more robust loop condition

--- web/src/HomePage.tsx
+++ web/src/HomePage.tsx
@@ -92,7 +92,7 @@ const LastProposals = () => {
     if (data !== undefined) {
       let newProposals = [];
       const fetchedProposals = data[0] as readonly PropcornProposal[];
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < Math.min(fetchedProposals.length, 5); i++) {
         if (fetchedProposals[i].status !== 0) {
           newProposals.push(fetchedProposals[i]);
         } else {
