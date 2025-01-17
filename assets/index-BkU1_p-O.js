35037e3881af87a688930b206530c218187b781c
Alberto Granzotto <alberto@granzotto.net>
2025-01-17 14:40:49 +0100

    Updates

--- assets/index-BkU1_p-O.js
+++ assets/index-BkU1_p-O.js
@@ -1,4 +1,4 @@
-import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-CEJj5hHC.js";import{r as ho,f as mo}from"./index-C0AY5iQU.js";/**
+import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-BgWbHR8L.js";import{r as ho,f as mo}from"./index-DPCeiuoJ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
