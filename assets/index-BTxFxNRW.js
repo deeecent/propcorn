342fe3e472ef3152c5b331f33406d4293ba8be90
Nicola Miotto <miotto@posteo.de>
2025-01-17 15:07:30 +0100

    Updates

--- assets/index-BTxFxNRW.js
+++ assets/index-BTxFxNRW.js
@@ -1,4 +1,4 @@
-import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-BgWbHR8L.js";import{r as ho,f as mo}from"./index-DPCeiuoJ.js";/**
+import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-fnIY9_bS.js";import{r as ho,f as mo}from"./index-CBAziamt.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
